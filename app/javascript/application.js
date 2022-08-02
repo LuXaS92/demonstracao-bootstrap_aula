aa20974186976385ee98431c3ca5cdf2124ecccb
LuXaS92 <109088944+LuXaS92@users.noreply.github.com>
2022-08-02 14:56:03 -0300

    aula_bootstrap

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails";
+import "controllers";
+import "channels";
+import  "bootstrap/dist/js/bootstrap.bundle";
+import "../assets/stylesheets/application.css";
